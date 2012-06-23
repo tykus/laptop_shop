bf97823cea93535bc666bbf77830fec188486215
tykus <brian.osullivan@live.com>
2012-06-23 09:46:41 +0100

    Parts I & II

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaptopShop::Application.config.secret_token = '522b3ff712f77dcfc30a8a6534740e0646b287d8a26782c7b29497fa72d019d4e88a05d7ff4c5cd2892acce9a1d5351fbb5f37d0652b8826b756b0d703da3cdc'
